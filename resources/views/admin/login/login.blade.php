4e832126cfda059760c4411460764712587a9c77
haider10954 <chhaider10954@gmail.com>
2022-11-21 15:00:41 +0500

    Login Button Added

--- resources/views/admin/login/login.blade.php
+++ resources/views/admin/login/login.blade.php
@@ -69,6 +69,9 @@
                                             <button class="btn btn-light " type="button" id="password-addon"><i class="mdi mdi-eye-outline"></i></button>
                                         </div>
                                     </div>
+                                    <div class="mt-3 d-grid">
+                                        <button class="btn btn-primary waves-effect waves-light" type="submit">Log In</button>
+                                    </div>
                                 </form>
                             </div>
 
