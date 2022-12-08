6b0dc2261e02880179cea6d5e2755ce1533ba414
haider10954 <chhaider10954@gmail.com>
2022-12-08 15:07:57 +0500

    Certificate ui changed

--- resources/views/admin/certificate/certificate.blade.php
+++ resources/views/admin/certificate/certificate.blade.php
@@ -52,7 +52,6 @@
     }
 
     .certificate_logo {
-        width: 138.67px;
         height: 39px;
         display: block;
         padding-bottom: 15px;
@@ -104,26 +103,26 @@
                 <hr class="hr-color" />
             </div>
             <div class="col-12" id="certificate">
-                <div class="position-relative">
+                <div class="position-relative w-75 m-auto">
                     <img src="{{ asset('assets/images/icons/frame.png')}}" class="w-100">
-                    <div class="position-absolute text-center w-100" style="padding: 10px 100px 10px 100px; top:50px;">
+                    <div class="position-absolute text-center w-100" style="padding: 10px 100px 10px 100px; top:15px;">
                         <div class="w-25 mx-auto">
                             <img src="{{ asset('assets/images/icons/certificate_header.png')}}" class="w-100">
-                            <hr class="divider" />
+                            <div class="divider mt-1"></div>
                         </div>
-                        <div class="certificate_header mb-3">CERTIFICATE</div>
-                        <div class="certificate_sub_title mb-3">OF COMPLETION</div>
+                        <div class="certificate_header mb-1">CERTIFICATE</div>
+                        <div class="certificate_sub_title">OF COMPLETION</div>
                         <div class="w-25 mx-auto">
                             <img src="{{ asset('assets/images/icons/certificate_bottom.png') }}" class="w-100">
-                            <hr class="divider" />
+                            <div class="divider mt-1"></div>
                         </div>
-                        <div class="certificate_name mb-3">
+                        <div class="certificate_name">
                             Hong Gil Dong
                         </div>
-                        <div class="certificate_description mb-3">
+                        <div class="certificate_description mb-1">
                             Is hereby cetified as an 2022 PTEdu Pelvic health Physiotherapy – <br /> Melissa Davidson with Full Certification for the half-year period <br /> starting on 2022/05/01 and ending on 2022/10/31
                         </div>
-                        <div class="d-flex justify-content-around align-items-center mt-5">
+                        <div class="d-flex justify-content-between align-items-center mt-3">
                             <div>
                                 <img src="{{ asset('assets/images/icons/certificate_logo.png') }}" class="certificate_logo">
                                 <img src="{{ asset('assets/images/index.png') }}" class="footer_logo" />
@@ -155,18 +154,16 @@
             const certificate = this.document.getElementById("certificate");
             var opt = {
 
-                margin: 1.25,
-                marginTop: 0.25,
+                margin: 1.5,
+                marginTop: 1,
                 filename: 'certificate.pdf',
-                image: {
-                    type: 'jpeg',
-                },
                 jsPDF: {
                     unit: 'in',
-                    orientation: 'landscape'
+                    format: 'A4',
+                    orientation: 'landscape',
                 }
             };
-            html2pdf().set(opt).from(certificate).save();
+            html2pdf().from(certificate).set(opt).save();
         })
     }
 </script>
