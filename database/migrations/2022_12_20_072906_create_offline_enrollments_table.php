78841f71bd65ade9b159ad1060e49fa9b16ac734
haider10954 <chhaider10954@gmail.com>
2022-12-20 12:38:26 +0500

    New Migrations Completed

---
+++ database/migrations/2022_12_20_072906_create_offline_enrollments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('offline_enrollments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('course_id')->constrained('offline_courses')->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('offline_enrollments');
+    }
+};
