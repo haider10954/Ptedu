ee0010098e7d1987282f3413a51bd945f599027d
haider10954 <chhaider10954@gmail.com>
2023-01-10 15:41:33 +0500

    Translation Added

--- lang/en/translation.php
+++ lang/en/translation.php
@@ -204,6 +204,8 @@ return [
     'Update' => 'Update',
     'User Info' => 'User Info',
     'To protect your personal information, please change your password, email, mobile phone number periodically.' => 'To protect your personal information, please change your password, email, mobile phone number periodically.',
+    'Change Password' => 'Change Password',
+
 
     // Admin Side Translation
     'Welcome Back' => 'Welcome Back',
