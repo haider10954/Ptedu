a5dbe2d3c688ec1d5b7f42c864155a5210e38adf
Haider Zaman <chhaider10954@gmail.com>
2023-01-17 13:21:29 +0500

    Update add_certificate.blade.php
    Updated a condition on Certificate

--- resources/views/admin/certificate/add_certificate.blade.php
+++ resources/views/admin/certificate/add_certificate.blade.php
@@ -110,7 +110,9 @@
             <div class="col-12">
                 <form method="post" action="{{ route('generate-certificate') }}">
                     @csrf
-                    <input type="hidden" name="id" value="{{ $certificate->id }}">
+                    @if(isset($certificate))
+                    <input type="hidden" name="id" value="{{ $certificate->id ?? '' }}">
+                    @endif
                     <input type="hidden" name="course_id" value="{{  Request::segment(4) }}">
                     <input type="hidden" name="user_id" value="{{  Request::segment(5) }}">
 
