424de87bee6ba6599085e4b34361b7c9597b0cc5
Haider Zaman <chhaider10954@gmail.com>
2023-01-20 18:44:06 +0500

    Update footer.blade.php
    Minor Change in Footer

--- resources/views/user/includes/footer.blade.php
+++ resources/views/user/includes/footer.blade.php
@@ -4,7 +4,7 @@
             <div class="d-flex align-items-center justify-content-between">
                 <div class="footer-left-content">
                     <h3 class="heading-h3 text-white mb-1">PTEdu<sub>®</sub></h3>
-                    <p class="text-white mb-1">{{ __('translation.Samseong-dong, Gangnam-gu, Seoul') }} 51-2</p>
+                    <p class="text-white mb-1">{{ __('translation.Samseong-dong, Gangnam-gu, Seoul') }}</p>
                     <p class="mb-0 text-white">C/S 02-6930-4105</p>
                 </div>
                 <div class="footer-right-content text-right">
