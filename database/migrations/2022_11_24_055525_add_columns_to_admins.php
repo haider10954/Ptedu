75449a24d34b774f2edfed9ac1aacc2e224230e2
Haider Zaman <chhaider10954@gmail.com>
2023-02-27 18:08:09 +0500

    Update 2022_11_24_055525_add_columns_to_admins.php

--- database/migrations/2022_11_24_055525_add_columns_to_admins.php
+++ database/migrations/2022_11_24_055525_add_columns_to_admins.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::table('admins', function (Blueprint $table) {
             $table->string('english_name')->nullable()->after('email');
-            $table->varchar('phone_number')->nullable()->after('english_name');
+            $table->text('phone_number')->nullable()->after('english_name');
             $table->string('job')->nullable()->after('phone_number');
             $table->text('profile_img')->nullable()->after('job');
             $table->longText('address')->nullable()->after('profile_img');
