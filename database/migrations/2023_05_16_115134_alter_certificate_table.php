2d31e2fa0bc2868abcc3a5391c07fe9d24a3fd19
Haider Zaman <chhaider10954@gmail.com>
2023-05-16 18:11:30 +0500

    New Migration Created For Upload Pdf

---
+++ database/migrations/2023_05_16_115134_alter_certificate_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('certificates', function (Blueprint $table) {
+            $table->dropColumn('course_duration');
+            $table->dropColumn('certificate_number');
+            $table->dropColumn('issue_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
