d03e638de5c4313389fe1a609e01a32c77d16805
Haider Zaman <chhaider10954@gmail.com>
2023-05-18 17:28:13 +0500

    Route Added

--- resources/views/admin/certificate/completed_student.blade.php
+++ resources/views/admin/certificate/completed_student.blade.php
@@ -152,7 +152,10 @@
                                 <span class="tutor_name">{{ $course->getCourses->getTutorName->english_name }}</span>
                             </td>
                             @if ($course->generate_certificate == 1)
-                            <td><a href="{{ route('certicate-view',$course->course_id) }}" disabled class="btn btn-status">수강완료</a></td>
+                            @php
+                            $check_certificate = Illuminate\Support\Facades\DB::table('certificates')->where('course_id', $course->course_id)->where('user_id',$course->user_id)->first();
+                            @endphp
+                            <td><a href="{{ route('certicate-view',$check_certificate->course_id) }}" disabled class="btn btn-status">수강완료</a></td>
                             @else
                             <td><a href="{{ route('add_certificate', [$course->getCourses->id , $course->getUser->id] )}}" class="btn btn-status">수료중</a></td>
                             @endif
