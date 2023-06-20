f16af61a57f7227c301543ae72a4ad992a2d8fc1
Haider Zaman <chhaider10954@gmail.com>
2023-06-20 17:14:07 +0500

    Point 12 fixed

--- resources/views/user/offline-lecture-detail.blade.php
+++ resources/views/user/offline-lecture-detail.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 <div class="section lecture_banner_section">
-    <div class="lecture_banner_img" style="background-image: url({{ asset('storage/offline_course/banner/' . $course_info->course_banner) }})"></div>
+    <div class="lecture_banner_img" style="  background-color: #191B2D;"></div>
     <div class="banner_text">
         @if ($embedded_video_url != false)
         {!! $embedded_video_url !!}
