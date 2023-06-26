00282cee240a53ac5d666eb93c787e80146d6e61
Haider Zaman <chhaider10954@gmail.com>
2023-06-26 14:09:33 +0500

    About Us Page Fix

--- resources/views/user/about_us.blade.php
+++ resources/views/user/about_us.blade.php
@@ -4,8 +4,8 @@
 
 @section('content')
 
-<div class="section pt-150" style="background-color: #000;">
-    <div class="container d-flex align-items-center justify-content-center">
+<div class="section pt-150 pb-0 pt-0" style="background-color: #000;">
+    <div class="container-fuild d-flex align-items-center justify-content-center">
         <div class="about_us_image">
             <img src="{{ asset('web_assets/images/PTEdu(web).png') }}" class="img-fluid h-100" alt="">
         </div>
