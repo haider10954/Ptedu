0b1eee4fe5f4995c7b8c421a892d7cdb9228c7a4
Haider Zaman <chhaider10954@gmail.com>
2023-06-26 18:19:51 +0500

    Online offline Done

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,8 +25,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if (\Schema::hasTable('courses')) {
-            $expert_courses = Course::with('getTutorName')->where('course_type', 'expert')->take(5)->get();
-            $public_courses = Course::with('getTutorName')->where('course_type', 'public')->take(5)->get();
+            $expert_courses = Course::with('getTutorName')->where('course_type', 'online')->take(5)->get();
+            $public_courses = Course::with('getTutorName')->where('course_type', 'offline')->take(5)->get();
             view()->share('online_expert_courses', $expert_courses);
             view()->share('online_public_courses', $public_courses);
         }
