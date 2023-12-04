431ede2f2fe123482e2231d3e4f387047cc51825
Iamqasimmansoor <qasimmansoor683@gmail.com>
2023-12-04 18:22:22 +0500

    Update layout.blade.php

--- resources/views/user/layout.blade.php
+++ resources/views/user/layout.blade.php
@@ -6,7 +6,10 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="naver-site-verification" content="8cba5f36e4afff1e7b37802fbe946fd0f84b08a3" />
-  <title>@yield('title')</title>
+  <meta property="og:type" content="website">   
+  <meta property="og:title" content="물리치료 관련 교육 플랫폼 피티에듀 - 대한중추신경계물리치료학회">
+  <meta property="og:description" content="최신 근거 기반, 임상 적용 물리치료 교육, PNF 써티 청구, 움직임 전문가, MAM, APPI, 필라테스, 보행">
+  <title>물리치료 관련 교육 플랫폼 피티에듀 - 대한중추신경계물리치료학회</title>
   <!-- App favicon -->
   <link rel="shortcut icon" href="{{ asset('assets/images/icons/favicon.png') }}">
   @yield('custom-stylesheet')
