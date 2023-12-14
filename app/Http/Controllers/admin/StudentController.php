307f93303b6ebbf4619a93191a7cec89b4508d63
Iamqasimmansoor <qasimmansoor683@gmail.com>
2023-12-14 19:36:51 +0500

    Update StudentController.php

--- app/Http/Controllers/admin/StudentController.php
+++ app/Http/Controllers/admin/StudentController.php
@@ -127,6 +127,6 @@ class StudentController extends Controller
     public function student_online_course_price_control($course_id){
         $records = Student_online_price_control::where('course_id', $course_id)->get();
         $users = User::get();
-        return view('admin.courses.student_online_course_price_control', compact('records','courses','users'));
+        return view('admin.courses.student_online_course_price_control', compact('records','users'));
     }
 }
