ddcc4c84bf9f08447cf7619ee5aa1dc10ab9f6eb
Haider Zaman <chhaider10954@gmail.com>
2023-12-20 16:18:15 +0500

    Create 2023_12_20_111707_add_course_schedule_to_online_enrollments.php

---
+++ database/migrations/2023_12_20_111707_add_course_schedule_to_online_enrollments.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('online_enrollments', function (Blueprint $table) {
+            $table->longText('course_schedule')->after('order_id')->nullable()->default(null);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('online_enrollments', function (Blueprint $table) {
+            //
+        });
+    }
+};
